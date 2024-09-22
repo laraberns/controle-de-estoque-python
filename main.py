5b64dc65c6d94f715b7e2631f16029d92d32a802
Lara Pereira <larabernspereira@gmail.com>
2024-09-22 18:41:48 -0300

    add exer 2

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 estoque = []
 
+estoque_inicial = "Notebook Dell;201;15;3200.00;4500.00#Notebook Lenovo;202;10;2800.00;4200.00#Mouse Logitech;203;50;70.00;150.00#Mouse Razer;204;40;120.00;250.00#Monitor Samsung;205;10;800.00;1200.00#Monitor LG;206;8;750.00;1150.00#Teclado Mecânico Corsair;207;30;180.00;300.00#Teclado Mecânico Razer;208;25;200.00;350.00#Impressora HP;209;5;400.00;650.00#Impressora Epson;210;3;450.00;700.00#Monitor Dell;211;12;850.00;1250.00#Monitor AOC;212;7;700.00;1100.00"
+
 # Cadastro de produtos
 def cadastrar_produto(descricao, codigo, quantidade_estoque, custo_item, preco_venda):
     for produto in estoque:
@@ -20,6 +22,19 @@ def cadastrar_produto(descricao, codigo, quantidade_estoque, custo_item, preco_v
     return novo_produto
 
 
-cadastrar_produto("Notebook Dell", 201, 15, 3200.00, 4500.00)
-cadastrar_produto("Notebook Lenovo", 201, 10, 2800.00, 4200.00) 
-cadastrar_produto("Mouse Logitech", 202, 50, 70.00, 150.00)
+# Adicionar Estoque inicial
+def adicionar_estoque_inicial():
+    lista_estoque_inicial = estoque_inicial.split('#')
+    
+    for produto in lista_estoque_inicial:
+        descricao, codigo, quantidade_estoque, custo_item, preco_venda = produto.split(';')
+        
+        codigo = int(codigo)
+        quantidade_estoque = int(quantidade_estoque)
+        custo_item = float(custo_item)
+        preco_venda = float(preco_venda)
+        
+        cadastrar_produto(descricao, codigo, quantidade_estoque, custo_item, preco_venda)
+
+adicionar_estoque_inicial()
+print(estoque)
